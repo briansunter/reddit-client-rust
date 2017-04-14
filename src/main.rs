8a1380e378bc46a64ba1d12ab28773800ac949a5
Brian Sunter <bsunter@gmail.com>
2017-04-13 19:58:43 -0700

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct Story {
     title: String,
     url: String,
     author: String,
-    ups : i64,
+    ups: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -36,23 +36,39 @@ struct Result {
 #[derive(Serialize, Deserialize, Debug)]
 struct Page {
     stories: Vec<Story>,
-    after : String,
+    after: String,
 }
 
-fn result_to_page(res:Result) -> Page {
-    let stories :Vec<Story> = res.data.children.into_iter().map(|s|s.data).collect();
-    return Page {stories : stories, after : res.data.after}
+fn result_to_page(res: Result) -> Page {
+    let stories: Vec<Story> = res.data.children.into_iter().map(|s| s.data).collect();
+    return Page {
+        stories: stories,
+        after: res.data.after,
+    };
 }
 
+trait RedditClient {
+    fn get_top_reddit_page(&self, after: String) -> Page;
+}
+
+struct RedditHTTPClient {}
+
+impl RedditClient for RedditHTTPClient {
+    fn get_top_reddit_page(&self, after: String) -> Page {
+        let ssl = NativeTlsClient::new().unwrap();
+        let connector = HttpsConnector::new(ssl);
+        let client = Client::with_connector(connector);
+        let url = format!("https://www.reddit.com/r/all/top.json?after={}", after);
+        let mut res = client.get(&url).send().unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        let reddit_result: Result = serde_json::from_str(&body).unwrap();
+        return result_to_page(reddit_result);
+    }
+}
 
 fn main() {
-    let ssl = NativeTlsClient::new().unwrap();
-    let connector = HttpsConnector::new(ssl);
-    let client = Client::with_connector(connector);
-    let mut res = client.get("https://www.reddit.com/r/all/top.json").send().unwrap();
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    let reddit_result: Result = serde_json::from_str(&body).unwrap();
-    let page = result_to_page(reddit_result);
+    let reddit_client = RedditHTTPClient {};
+    let page = reddit_client.get_top_reddit_page("foo".to_owned());
     println!("{:?}", page);
 }
